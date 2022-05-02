1c549831a6fcbb9eeb7e33b6c1c8b1ec5af45b79
Ahmed Rifat <rifatbafsc@gmail.com>
2022-05-02 16:51:26 +0600

    db localhost items

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
@@ -6,6 +7,30 @@ const app =express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgjfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+      try{
+         await client.connect();
+         const itemsCollection = client.db('tajfurniture').collection('items');
+         
+         app.get('/items', async(req,res)=>{
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+         })
+
+
+      }finally{
+
+      }
+}
+
+run().catch(console.dir);
+
 
 app.get('/',(req, res)=>{
      res.send('running server');
