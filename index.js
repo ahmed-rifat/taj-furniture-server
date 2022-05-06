2b3e24d5c92a94354064cbeacd01761fd1b3cb72
Ahmed Rifat <rifatbafsc@gmail.com>
2022-05-06 19:06:42 +0600

    item delete API Created

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ async function run(){
              res.send(item);
          })
 
+         app.delete('/items/:id', async(req, res)=>{
+             const id = req.params.id;
+             const query = {_id: ObjectId(id)};
+             const result = await itemsCollection.deleteOne(query);
+             res.send(result);
+         })
+
 
       }finally{
 
